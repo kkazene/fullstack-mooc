59629f4fa6c4139d4e8bb98755ab2ffb3f372055
Tuuli Sarantola <tuuli.sarantola@gmail.com>
2018-12-13 16:59:04 +0200

    Exercise 4.9

--- Osa4/tests/blog_api.test.js
+++ Osa4/tests/blog_api.test.js
@@ -75,6 +75,29 @@ describe('API tests', () => {
 
     expect(response.body.length).toBe(initialBlogs.length)
   })
+
+  test('a valid blog can be added ', async () => {
+    const newBlog = {
+      title: 'A Tale of Dudes',
+      author: 'Dude McDudeface',
+      url: 'http://dude.face.com/yes/itsveryreal.html',
+      likes: 0
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api
+      .get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+
+    expect(response.body.length).toBe(initialBlogs.length + 1)
+    expect(titles).toContain('A Tale of Dudes')
+  })
 })
 
 afterAll(() => {
