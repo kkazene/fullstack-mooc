135bae58cef2aaebb776d80986bb356818406b8e
Tuuli Sarantola <tuuli.sarantola@gmail.com>
2018-12-13 17:28:38 +0200

    Add try catch block around requests

--- Osa4/controllers/blogs.js
+++ Osa4/controllers/blogs.js
@@ -2,20 +2,29 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs)
+  try {
+    const blogs = await Blog.find({})
+    response.json(blogs)
+  } catch (exception) {
+    console.log(exception)
+    response.status(500).json({ error: 'something went wrong' })
+  }
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const body = request.body
-  if (body.title === undefined || body.url === undefined)
-    return response.status(400).json({ error: 'content missing' })
-  if (body.likes === undefined)
-    body.likes = 0
-  const blog = new Blog(body)
-  const savedBlog = await blog.save()
+  try {
+    const body = request.body
+    if (body.title === undefined || body.url === undefined)
+      return response.status(400).json({ error: 'content missing' })
+    if (body.likes === undefined)
+      body.likes = 0
+    const blog = new Blog(body)
+    const savedBlog = await blog.save()
 
-  response.status(201).json(savedBlog)
+    response.status(201).json(savedBlog)
+  } catch (exception) {
+    response.status(500).json({ error: 'something went wrong' })
+  }
 })
 
 module.exports = blogsRouter
