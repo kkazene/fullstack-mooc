a006cbaa45717dd2541a83ff1bd4e57b6dc807aa
Tuuli Sarantola <tuuli.sarantola@gmail.com>
2018-12-13 14:53:02 +0200

    Exercise 4.8

--- Osa4/index.js
+++ Osa4/index.js
@@ -5,18 +5,30 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 
-if ( process.env.NODE_ENV !== 'production' ) {
-  require('dotenv').config()
-}
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose
+  .connect(config.mongoUrl)
+  .then( () => {
+    console.log('connected to database', config.mongoUrl)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const server = http.createServer(app)
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
 })
+
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app, server
+}
--- Osa4/tests/list_helper.test.js
+++ Osa4/tests/list_helper.test.js
@@ -61,76 +61,78 @@ const biggerList = [
   }
 ]
 
-test('dummy is called', () => {
-  const result = listHelper.dummy(emptyList)
-  expect(result).toBe(1)
-})
-
-describe('total likes', () => {
-  test('of an empty list is zero', () => {
-    const result = listHelper.totalLikes(emptyList)
-    expect(result).toBe(0)
-  })
-  test('when list has only one blog equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    expect(result).toBe(5)
-  })
-  test('of a bigger list is calculated right', () => {
-    const result = listHelper.totalLikes(biggerList)
-    expect(result).toBe(36)
+describe.skip('list helpers', () => {
+  test('dummy is called', () => {
+    const result = listHelper.dummy(emptyList)
+    expect(result).toBe(1)
   })
 
-})
+  describe('total likes', () => {
+    test('of an empty list is zero', () => {
+      const result = listHelper.totalLikes(emptyList)
+      expect(result).toBe(0)
+    })
+    test('when list has only one blog equals the likes of that', () => {
+      const result = listHelper.totalLikes(listWithOneBlog)
+      expect(result).toBe(5)
+    })
+    test('of a bigger list is calculated right', () => {
+      const result = listHelper.totalLikes(biggerList)
+      expect(result).toBe(36)
+    })
 
-describe('favorite blog', () => {
+  })
 
-  test('of an empty list is empty', () => {
-    const result = listHelper.favoriteBlog(emptyList)
-    expect(result).toEqual({})
-  })
-  test('when list has only one blog equals that', () => {
-    const result = listHelper.favoriteBlog(listWithOneBlog)
-    expect(result).toEqual({ title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      likes: 5 })
-  })
-  test('of a bigger list is calculated right', () => {
-    const result = listHelper.favoriteBlog(biggerList)
-    expect(result).toEqual({ title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      likes: 12 })
-  })
-})
+  describe('favorite blog', () => {
 
-describe('most blogs', () => {
-  test('of an empty list is empty', () => {
-    const result = listHelper.mostBlogs(emptyList)
-    expect(result).toEqual({})
+    test('of an empty list is empty', () => {
+      const result = listHelper.favoriteBlog(emptyList)
+      expect(result).toEqual({})
+    })
+    test('when list has only one blog equals that', () => {
+      const result = listHelper.favoriteBlog(listWithOneBlog)
+      expect(result).toEqual({ title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        likes: 5 })
+    })
+    test('of a bigger list is calculated right', () => {
+      const result = listHelper.favoriteBlog(biggerList)
+      expect(result).toEqual({ title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        likes: 12 })
+    })
   })
-  test('when list has only one blog equals that', () => {
-    const result = listHelper.mostBlogs(listWithOneBlog)
-    expect(result).toEqual({ author: 'Edsger W. Dijkstra',
-      blogs: 1 })
+
+  describe('most blogs', () => {
+    test('of an empty list is empty', () => {
+      const result = listHelper.mostBlogs(emptyList)
+      expect(result).toEqual({})
+    })
+    test('when list has only one blog equals that', () => {
+      const result = listHelper.mostBlogs(listWithOneBlog)
+      expect(result).toEqual({ author: 'Edsger W. Dijkstra',
+        blogs: 1 })
+    })
+    test('of a bigger list is calculated right', () => {
+      const result = listHelper.mostBlogs(biggerList)
+      expect(result).toEqual({ author: 'Robert C. Martin',
+        blogs: 3 })
+    })
   })
-  test('of a bigger list is calculated right', () => {
-    const result = listHelper.mostBlogs(biggerList)
-    expect(result).toEqual({ author: 'Robert C. Martin',
-      blogs: 3 })
-  })
-})
-describe('most likes', () => {
-  test('of an empty list is empty', () => {
-    const result = listHelper.mostLikes(emptyList)
-    expect(result).toEqual({})
-  })
-  test('when list has only one blog equals that', () => {
-    const result = listHelper.mostLikes(listWithOneBlog)
-    expect(result).toEqual({ author: 'Edsger W. Dijkstra',
-      likes: 5 })
-  })
-  test('of a bigger list is calculated right', () => {
-    const result = listHelper.mostLikes(biggerList)
-    expect(result).toEqual({ author: 'Edsger W. Dijkstra',
-      likes: 17 })
+  describe('most likes', () => {
+    test('of an empty list is empty', () => {
+      const result = listHelper.mostLikes(emptyList)
+      expect(result).toEqual({})
+    })
+    test('when list has only one blog equals that', () => {
+      const result = listHelper.mostLikes(listWithOneBlog)
+      expect(result).toEqual({ author: 'Edsger W. Dijkstra',
+        likes: 5 })
+    })
+    test('of a bigger list is calculated right', () => {
+      const result = listHelper.mostLikes(biggerList)
+      expect(result).toEqual({ author: 'Edsger W. Dijkstra',
+        likes: 17 })
+    })
   })
 })
