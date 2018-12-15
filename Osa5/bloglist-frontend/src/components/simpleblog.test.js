92bb86202311ca28739fdae890d31a14dc274891
Tuuli Sarantola <tuuli.sarantola@gmail.com>
2018-12-15 15:55:14 +0200

    Exercise 5.13

--- Osa5/bloglist-frontend/src/components/simpleblog.test.js
+++ Osa5/bloglist-frontend/src/components/simpleblog.test.js
@@ -17,5 +17,19 @@ describe.only('Render simpleblog', () => {
     expect(contentDiv.text()).toContain(blog.title)
     expect(contentDiv.text()).toContain(blog.author)
     expect(likesDiv.text()).toContain(blog.likes)
+  }),
+  it('calls function twice when button is clicked twice', () => {
+    const blog = {
+      title: 'Yes it\'s a title',
+      author: 'Itsa Me Mario',
+      likes: 5
+    }
+    const mockHandler = jest.fn()
+    const blogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler} />)
+    
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 })
