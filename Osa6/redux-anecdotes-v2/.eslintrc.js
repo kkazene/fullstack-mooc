2b97d291809029faa3d5b66fbebaac2c2b1ae889
Tuuli Sarantola <tuuli.sarantola@gmail.com>
2018-12-15 21:43:12 +0200

    Exercise 6.1

---
+++ Osa6/redux-anecdotes-v2/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jest/globals": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react", "jest"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "eqeqeq": "error",
+        "no-trailing-spaces": "error",
+        "arrow-spacing": [
+            "error", { "before": true, "after": true }
+        ],
+        "react/prop-types": 0
+    }
+};
